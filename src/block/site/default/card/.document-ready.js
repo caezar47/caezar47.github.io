73ac5c52a2638a82bf5f57de31e0c525fffe919a
caezar47 <iss47@ya.ru>
2019-02-11 12:18:22 +0300

    fix works

--- src/block/site/default/card/.document-ready.js
+++ src/block/site/default/card/.document-ready.js
@@ -1,11 +1,11 @@
-var btn_site = $('.card__item.is--site[href="#"]');
+var btn_site = $('.btn__item.is--site[href="#"]');
 var btn_github = $('.card__item.is--github[href="#"]');
 btn_site.remove();
 btn_github.remove();
-/*
-btn_site.removeAttr("target");
+
+/*btn_site.removeAttr("target");
 btn_site.on("click", function () {
-    alert("Извините сайт снят с размещения");
+    alert("Извините сайт недоступен.");
 })
 btn_github.removeAttr("target");
 btn_github.on("click", function () {
