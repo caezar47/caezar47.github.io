14c8f1379904b5a25988a26ad095e35336f15fca
caezar47 <iss47@ya.ru>
2019-09-16 11:47:53 +0300

    add new work

--- src/block/site/page/.document-ready.js
+++ src/block/site/page/.document-ready.js
@@ -1,4 +1,17 @@
- var container = $('#portfolio_wrapper');
+$("[data-filter]").click(function(){
+    var value = $(this).attr("data-filter");
+    var elem = $("[data-item]");
+    if(value == "all"){
+        $(elem).addClass("is--visible");
+    }
+    else{
+        $(elem).not("."+value).removeClass("is--visible").addClass("is--hidden");
+        $(elem).filter("."+value).removeClass("is--hidden").addClass("is--visible");
+    }
+    $("[data-filter]").parent().removeClass("active");
+    $(this).parent().addClass("active");
+});
+/* var container = $('#portfolio_wrapper');
 
  
     container.isotope({
@@ -63,3 +76,4 @@
     $(window).bind('resize', function() {
         setProjects();
     });
+*/
